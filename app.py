a9e536a93c30ef4a29034cd1d0f7af01b687b10d
adeepthinarayan <deepthi.narayan@thinknyx.com>
2025-08-01 02:08:52 +0530

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import boto3
 import os
 from werkzeug.utils import secure_filename
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/tmp'
 
 # ---------- AWS S3 CONFIG ----------
-S3_BUCKET = 'ihtpeed'
-S3_REGION = 'ap-south-1'
+S3_BUCKET = 'ihtpeed'  # Replace with your bucket name
+S3_REGION = 'ap-south-1'  # Replace with your region
 s3 = boto3.client('s3', region_name=S3_REGION)
 
 # ---------- MYSQL RDS CONFIG ----------
@@ -21,9 +21,24 @@ DB_CONFIG = {
     'database': 'babycontest'
 }
 
+def insert_to_db(baby_name, baby_age, parent_name, contact, image_url):
+    try:
+        connection = mysql.connector.connect(**DB_CONFIG)
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO entries (baby_name, baby_age, parent_name, contact, s3_image_url)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (baby_name, baby_age, parent_name, contact, image_url))
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Error as e:
+        print("Database error:", e)
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_form():
     if request.method == 'POST':
+        # Get form fields
         baby_name = request.form['baby_name']
         baby_age = request.form['baby_age']
         parent_name = request.form['parent_name']
@@ -36,37 +51,24 @@ def upload_form():
             file.save(local_path)
 
             # Upload to S3
-            s3.upload_file(local_path, S3_BUCKET, filename)
-            image_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
-
-            # Save to DB
             try:
-                connection = mysql.connector.connect(**DB_CONFIG)
-                cursor = connection.cursor()
-                cursor.execute("""
-                    INSERT INTO entries (baby_name, baby_age, parent_name, contact, s3_image_url)
-                    VALUES (%s, %s, %s, %s, %s)
-                """, (baby_name, baby_age, parent_name, contact, image_url))
-                connection.commit()
-                cursor.close()
-                connection.close()
-            except Error as e:
-                return f"Database error: {str(e)}"
+                s3.upload_file(local_path, S3_BUCKET, filename)
+                image_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
+            except Exception as e:
+                return f"Upload failed: {str(e)}"
 
-            return redirect(url_for('submission_success', baby_name=baby_name, baby_age=baby_age,
-                                    parent_name=parent_name, contact=contact, image_url=image_url))
-    return render_template('form.html')
+            # Insert into DB
+            insert_to_db(baby_name, baby_age, parent_name, contact, image_url)
 
+            return render_template('form.html',
+                                   submitted=True,
+                                   baby_name=baby_name,
+                                   baby_age=baby_age,
+                                   parent_name=parent_name,
+                                   contact=contact,
+                                   image_url=image_url)
 
-@app.route('/success')
-def submission_success():
-    return render_template('success.html',
-                           baby_name=request.args.get('baby_name'),
-                           baby_age=request.args.get('baby_age'),
-                           parent_name=request.args.get('parent_name'),
-                           contact=request.args.get('contact'),
-                           image_url=request.args.get('image_url'))
-
+    return render_template('form.html', submitted=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
